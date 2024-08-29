aef4133ef1dba22e45ae99779686e3b146067b42
Deividi <deividi.contato@gmail.com>
2024-08-29 14:01:03 -0300

    adição inicial de cards

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 // Função para servir a página inicial
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("views/profile_views/home.html", "views/components/header.html"))
+    tmpl := template.Must(template.ParseFiles(
+        "views/profile_views/home.html",
+        "views/components/header.html",
+        "views/components/professional_card.html",
+    ))
     tmpl.ExecuteTemplate(w, "home.html", nil)
 }
 
