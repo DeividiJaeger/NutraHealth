e793e9576a63a719a8cd673d7659ff8523db0ea7
Deividi <deividi.contato@gmail.com>
2024-09-04 12:23:38 -0300

    Criado rota para pagina de registro de usuarios, andamento para tornar os cards dinamicos com base nos dados coletados do backend

--- main.go
+++ main.go
@@ -11,8 +11,18 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
         "views/profile_views/home.html",
         "views/components/header.html",
         "views/components/professional_card.html",
+        "views/profile_views/registration.html",
     ))
     tmpl.ExecuteTemplate(w, "home.html", nil)
+}
+
+// Função para servir a página de cadastro
+func registrationHandler(w http.ResponseWriter, r *http.Request) {
+    tmpl := template.Must(template.ParseFiles(
+        "views/profile_views/registration.html",
+        "views/components/header.html",
+    ))
+    tmpl.ExecuteTemplate(w, "registration.html", nil)
 }
 
 func main() {
@@ -24,6 +34,9 @@ func main() {
     // Rota para a página inicial
     mux.HandleFunc("/", homeHandler)
 
+    // Rota para a pagina de resitrar
+    mux.HandleFunc("/registration", registrationHandler)
+    
     // Iniciar o servidor
     http.ListenAndServe(":8080", mux)
 }
